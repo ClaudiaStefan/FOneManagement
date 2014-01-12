42250ec84ce47d2d32bef6f7be42e1d553824ec6
claudia.stefan <claudeevine@yahoo.com>
2014-01-12 16:20:39 +0200

    First commit
    Empty project - work to be done

---
+++ Proiect Afaceri Virtuale - JOC/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proiect_Afaceri_Virtuale.Startup))]
+namespace Proiect_Afaceri_Virtuale
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
